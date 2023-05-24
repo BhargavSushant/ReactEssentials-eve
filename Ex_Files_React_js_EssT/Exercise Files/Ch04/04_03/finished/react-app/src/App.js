fa9a0b6dafd54923d609564b0bbd963d40f075f9
BhargavSushant <BhargavSushant@gmail.com>
2023-05-24 09:58:41 -0400

    useEffect usage

--- Ex_Files_React_js_EssT/Exercise Files/Ch04/04_03/finished/react-app/src/App.js
+++ Ex_Files_React_js_EssT/Exercise Files/Ch04/04_03/finished/react-app/src/App.js
@@ -1,18 +1,28 @@
 import "./App.css";
+import { useState, useEffect } from "react";
 
-const [firstCity, second] = [
-  "Tokyo",
-  "Tahoe City",
-  "Bend"
-];
+// const [firstCity, second] = [
+//   "Tokyo",
+//   "Tahoe City",
+//   "Bend"
+// ];
 
-console.log(firstCity);
-console.log(second);
+// console.log(firstCity);
+// console.log(second);
 
-function App({ library }) {
+function App({ currentEmotion }) {
+  const [emotion, setEmotion] = useState("Happy");
+
+  useEffect(() => {
+    console.log(`It's ${emotion} around here!`);
+  }, [emotion]);
+
   return (
     <div className="App">
-      <h1>Hello from {library}</h1>
+      <h1>current emotion is {emotion}</h1>
+      <button onClick={() => setEmotion("sad")}>SAD</button>
+      <button onClick={() => setEmotion("happy")}>happy</button>
+      <button onClick={() => setEmotion("confused")}>CONFUSED</button>
     </div>
   );
 }
